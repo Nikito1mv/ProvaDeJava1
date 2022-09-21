fc9eb315509e208bf54340c7da83692ef79311c1
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-09-21 16:54:50 -0300

    q1
    terminando 1 questão

--- src/Questao1/Agenda.java
+++ src/Questao1/Agenda.java
@@ -4,6 +4,8 @@
  */
 package Questao1;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Micael
@@ -29,6 +31,57 @@ public class Agenda {
         if (cheio) {
             System.out.println("Agenda cheia");
         }
+    }
+    
+    public void removePessoa(String nome) {
+        for(int i = 0; i < agenda.length; i++) {
+            if(agenda[i].getNome().equals(nome)) {
+               agenda[i] = null;
+               break;
+            }
+        }
+    }
+    
+    public int buscaPessoa(String nome) {
+        for(int i = 0; i < agenda.length; i++) {
+            if(agenda[i].getNome().equals(nome)) {
+               return i;
+            } else {
+                System.out.println("Pessoa não encontrada");
+            }
+        } return 11;
+    }
+    
+    public void imprimeAgenda() {
+        /*for(int i = 0; i < agenda.length; i++) {
+            agenda[i].toString();
+        }*/
+        System.out.println(Arrays.toString(agenda));
+    }
+    
+    public void imprimePessoa(int index) {
+        for(int i = 0; i < agenda.length; i++) {
+            if(i == index) {
+                agenda[i].toString();
+            }
+        }
+    }
+    
+    
+    
+    public void armazenaPessoas(Pessoa nomedapessoa) {
+        boolean cheio = true;
+        for (int i = 0; i < agenda.length; i++) {
+            if (agenda[i] == null) {
+                agenda[i] = nomedapessoa;
+                cheio = false;
+                break;
+            }
+        }
+        if (cheio) {
+            System.out.println("Agenda cheia");
+        }
+
     }
     
 }
--- src/Questao1/Main.java
+++ src/Questao1/Main.java
@@ -10,8 +10,27 @@ package Questao1;
  */
 public class Main {
     public static void main(String[] args) {
+        /*
         Pessoa micael = new Pessoa("Micael", 20, 1.81);
         System.out.println(micael);
+        */
+        Agenda ag = new Agenda();
+        Pessoa micael = new Pessoa("Micael", 20, 1.81);
+        ag.armazenaPessoa("Hyago", 24, 1.90);
+        ag.armazenaPessoa("João", 18, 1.78);
+        ag.armazenaPessoa("Jean", 17, 1.78);
+        ag.armazenaPessoa("Negão Cavalo", 18, 1.76);
+        ag.armazenaPessoa("John Animes", 20, 1.83);
+        ag.armazenaPessoa("Alexandre", 47, 1.80);
+        ag.armazenaPessoa("Luciene", 45, 1.68);
+        ag.armazenaPessoa("Giovani", 14, 1.78);
+        ag.armazenaPessoa("Bruna", 15, 1.69);
+        ag.armazenaPessoas(micael);
+        ag.imprimeAgenda();
+        ag.armazenaPessoa("Kauan", 19, 1.82);
+        ag.imprimePessoa(ag.buscaPessoa("Hyago"));
+        ag.removePessoa("Giovani");
+        ag.imprimeAgenda();
     }
     
 }
