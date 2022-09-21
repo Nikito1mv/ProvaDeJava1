0ed93a119ddc69626510aae88d36cc4e39491fa7
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-09-21 06:24:14 -0300

    first commit

---
+++ src/Questao1/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao1;
+
+/**
+ *
+ * @author Micael
+ */
+public class Main {
+    public static void main(String[] args) {
+        Pessoa micael = new Pessoa("Micael", 20, 1.81);
+        System.out.println(micael);
+    }
+    
+}
---
+++ src/Questao1/Pessoa.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao1;
+
+/**
+ *
+ * @author Micael
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    
+    @Override
+    public String toString() {
+        return "{Nome: " + nome + ", Idade: " + idade + ", Altura: " + altura + "}";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+}
