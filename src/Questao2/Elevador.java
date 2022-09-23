faf15cd26b1e30bc40cb7b37559d62dcbabf84fe
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-09-23 14:23:23 -0300

    readme

--- src/Questao2/Elevador.java
+++ src/Questao2/Elevador.java
@@ -10,5 +10,23 @@ package Questao2;
  * @author PC
  */
 public class Elevador {
+    private int andarAtual;
+    private int[] totalAndares;
+    private int[] capacidadePessoas;
+    private int numeroPessoas;
+    
+    public Elevador(int totalDeAndares, int capacidadeDePessoas) {
+        this.andarAtual = 0;
+        totalAndares = new int[totalDeAndares];
+        capacidadePessoas = new int[capacidadeDePessoas];
+        this.numeroPessoas = 0;
+    }
+    
+    public void Entrar() {
+        boolean cheio = true;
+        for (int i = 0; i < capacidadePessoas.length; i++){
+            
+        }
+    }
     
 }
