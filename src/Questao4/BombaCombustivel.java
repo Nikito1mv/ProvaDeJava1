26d126f8bb3ea871ac4add06d37c7d5a759e9418
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-09-26 01:17:29 -0300

    q4
    atualizando a questão 4

---
+++ src/Questao4/BombaCombustivel.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao4;
+
+/**
+ *
+ * @author Micael
+ */
+public class BombaCombustivel {
+    private int id;
+    private String tipoDeCombustivel;
+    private double valorDoLitro;
+    private double quantidadeDoCombustivel;
+    
+    BombaCombustivel(int id, String tipoDeCombustivel, double valorDoLitro, double quantidadeDoCombustivel){
+        this.id = id;
+        this.tipoDeCombustivel = tipoDeCombustivel;
+        this.valorDoLitro = valorDoLitro;
+        this.quantidadeDoCombustivel = quantidadeDoCombustivel;
+    }
+    
+    public void abastecerPorValor(double valor) {
+        double litros = valor / valorDoLitro;
+        quantidadeDoCombustivel -= litros;
+        System.out.println("Você abasteceu " + litros + " litros");
+    }
+    
+    public void abastecerPorLitro(double litros) {
+        double valor = litros * valorDoLitro;
+        quantidadeDoCombustivel -= litros;
+        System.out.println("Total a pagar é " + valor + " reais");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTipoDeCombustivel() {
+        return tipoDeCombustivel;
+    }
+
+    public double getValorDoLitro() {
+        return valorDoLitro;
+    }
+
+    public double getQuantidadeDoCombustivel() {
+        return quantidadeDoCombustivel;
+    }
+
+    public void setTipoDeCombustivel(String tipoDeCombustivel) {
+        this.tipoDeCombustivel = tipoDeCombustivel;
+    }
+
+    public void setValorDoLitro(double valorDoLitro) {
+        this.valorDoLitro = valorDoLitro;
+    }
+
+    public void setQuantidadeDoCombustivel(double quantidadeDoCombustivel) {
+        this.quantidadeDoCombustivel = quantidadeDoCombustivel;
+    }
+    
+}
